70d31159e3fe379eccf21f86894ed901d5494011
Cauã Sebastian <cauasebastian2207@gmail.com>
2024-11-05 23:01:42 +0000

    initial commit

---
+++ procesy/src/main/java/com/procesy/procesy/ProcesyApplication.java
@@ -0,0 +1,13 @@
+package com.procesy.procesy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProcesyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProcesyApplication.class, args);
+	}
+
+}
---
+++ procesy/src/test/java/com/procesy/procesy/ProcesyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.procesy.procesy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProcesyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
