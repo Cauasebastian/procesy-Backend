e247324bc19ce6657d181d5c7a039053ee4aa00a
Cauã Sebastian <133614515+Cauasebastian@users.noreply.github.com>
2024-12-11 17:03:34 -0300

    Fix: Fix CORS Error

--- procesy/src/main/java/com/procesy/procesy/security/SecurityConfig.java
+++ procesy/src/main/java/com/procesy/procesy/security/SecurityConfig.java
@@ -12,6 +12,11 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 @Configuration
 @EnableWebSecurity
@@ -34,10 +39,11 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable()) // Desativa CSRF
+                .cors(cors -> cors.configurationSource(corsConfigurationSource())) // Habilita CORS com a configuração definida
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/auth/**", "/api/**",  "/swagger-ui/**",
+                        .requestMatchers("/auth/**", "/api/**", "/swagger-ui/**",
                                 "/v3/api-docs/**", "/swagger-resources/**",
-                                "/webjars/**", "/v2/api-docs/**").permitAll() // Permite acesso público às rotas de autenticação
+                                "/webjars/**", "/v2/api-docs/**").permitAll() // Permite acesso público às rotas de autenticação e documentação
                         .anyRequest().authenticated() // Todas as outras rotas requerem autenticação
                 )
                 .sessionManagement(session -> session
@@ -51,5 +57,17 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:5173")); // Permite apenas o frontend específico
+        configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS")); // Métodos HTTP permitidos
+        configuration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type")); // Cabeçalhos permitidos
+        configuration.setAllowCredentials(true); // Permite o envio de credenciais (cookies, headers)
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration); // Aplica a configuração para todas as rotas
+        return source;
     }
 }
